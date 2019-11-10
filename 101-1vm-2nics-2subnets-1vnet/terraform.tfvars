b14de032cf398b66746cf688e2c64e7c6537285a
kpatnayakuni <kiran@patnayakuni.in>
2019-11-10 15:12:53 +0530

    Initial Commit

---
+++ 101-1vm-2nics-2subnets-1vnet/terraform.tfvars
@@ -0,0 +1,6 @@
+subscription_id = "e0d639dd-ce7b-4173-92a6-8881e5c5d40a"
+client_id       = "669d4aeb-76a2-4572-9979-6f68b0b0d668"
+client_secret   = "4e377541-f4d3-44bf-9f09-5e2a87a9e028"
+tenant_id       = "9dfb19bc-3565-411e-a0ee-cc1a3a6cafa3"
+admin_username  = "sysadmin"
+admin_password  = "PowerShell5.0"
---
+++ 101-1vm-2nics-2subnets-1vnet/variables.tf
@@ -0,0 +1,60 @@
+provider "azurerm" {
+    subscription_id = "${var.subscription_id}"
+    client_id       = "${var.client_id}"
+    client_secret   = "${var.client_secret}"
+    tenant_id       = "${var.tenant_id}"
+}
+
+variable "subscription_id" {
+    description = "Enter Subscription ID for provisioning resources in Azure"
+}
+
+variable "client_id" {
+    description = "Enter Client ID for Application created in Azure AD"
+}
+
+variable "client_secret" {
+    description = "Enter Client secret for Application created in Azure AD"
+}
+
+variable "tenant_id" {
+    description = "Enter Tenant ID /Directory ID of your Azure AD. Run Get-AzSubscription in your PowerShell Window to get the Tenant ID."
+}
+
+variable "resource_group_name" {
+    type = "string"
+    default = "terraform-rg"
+    description = "Enter the resource group name"
+}
+
+variable "virtual_machine_size" {
+    type = "string"
+    default = "Standard_DS1_v2"
+    description = "Virtual machine size (has to be at least the size of Standard_A3 to support 2 NICs)"
+}
+
+#variable "admin_username" {
+#    type = "string"
+#    default = "${var.admin_username}"
+#    description= "Default Admin username"
+#}
+
+#variable "admin_password" {
+#    type = "string"
+#    description = "Default Admin password"
+#}
+
+variable "storage_account_type" {
+    type = "string"
+    default = "Standard"
+    description = "Storage Account type for the VM and VM diagnostic storage"
+}
+variable "location" {
+    type = "string"
+    default = "westus"
+    description = "Enter the location for all resources."
+}
+
+
+
+
