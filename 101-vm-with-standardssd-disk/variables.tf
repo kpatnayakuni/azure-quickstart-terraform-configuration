de024133f4450471a704296351d27fbb0100a9df
AnushaAD <anusha.au011@gmail.com>
2020-08-25 20:43:54 +0530

    modified the format

--- 101-vm-with-standardssd-disk/variables.tf
+++ 101-vm-with-standardssd-disk/variables.tf
@@ -1,71 +1,71 @@
 # Authentication
 provider "azurerm" {
-    features {}
-     subscription_id = var.tf_var_arm_subscription_id
-     client_id  = var.tf_var_arm_client_id
-     client_secret = var.tf_var_arm_client_secret
-     tenant_id = var.tf_var_arm_tenant_id
+  features {}
+  subscription_id = var.tf_var_arm_subscription_id
+  client_id       = var.tf_var_arm_client_id
+  client_secret   = var.tf_var_arm_client_secret
+  tenant_id       = var.tf_var_arm_tenant_id
 }
 
 # Variable declaration
 variable "tf_var_arm_subscription_id" {
-    description = "enter subscription id"
+  description = "enter subscription id"
 }
 
 variable "tf_var_arm_client_id" {
-    description = "Enter Client ID"
+  description = "Enter Client ID"
 }
 
 variable "tf_var_arm_client_secret" {
-    description = "Enter secret"
+  description = "Enter secret"
 }
 
 variable "tf_var_arm_tenant_id" {
-    description = "Enter tenant ID"
+  description = "Enter tenant ID"
 }
 
 variable "resourceGroupName" {
-    type        = string
-    default     = "demo-rg"
-    description = "Resource Group for this deployment."
+  type        = string
+  default     = "demo-rg"
+  description = "Resource Group for this deployment."
 }
 
 variable "location" {
-    type        = string
-    default     = "WestUS"
-    description = "Location for all resources"
+  type        = string
+  default     = "WestUS"
+  description = "Location for all resources"
 }
 
 variable "virtualMachineName" {
-    type = string
-    default = "demomachine"
-    description = "The name of the VM"
+  type        = string
+  default     = "demomachine"
+  description = "The name of the VM"
 }
 
 variable "adminUsername" {
-    type        = string
-    description = "The admin user name of the VM."
+  type        = string
+  description = "The admin user name of the VM."
 }
 
 variable "adminPassword" {
-    type        = string
-    description = "The admin password of the VM."
+  type        = string
+  description = "The admin password of the VM."
 }
 
 variable "disktype" {
-    type        = list
-    default     = ["StandardSSD_LRS","Standard_LRS","Premium_LRS"]
-    description = "The Storage type of the data Disks."
+  type        = list
+  default     = ["StandardSSD_LRS", "Standard_LRS", "Premium_LRS"]
+  description = "The Storage type of the data Disks."
 }
 
 variable "virtualMachineSize" {
-    type = string
-    default = "Standard_DS3_V2"
-    description = "The virtual machine size. Enter a Premium capable VM size if DiskType is entered as Premium_LRS."
+  type        = string
+  default     = "Standard_DS3_V2"
+  description = "The virtual machine size. Enter a Premium capable VM size if DiskType is entered as Premium_LRS."
 }
 
 variable "windowsOSVersion" {
-    type        = list
-    default     = ["2008-R2-SP1","2012-Datacenter","2012-R2-Datacente","2016-Datacenter"]
-    description = "The Windows version for the VM."
+  type        = list
+  default     = ["2008-R2-SP1", "2012-Datacenter", "2012-R2-Datacente", "2016-Datacenter"]
+  description = "The Windows version for the VM."
 }
