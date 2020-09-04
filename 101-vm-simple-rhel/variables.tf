1b552d6028761fd0dfcdd74445fb8564a4009c18
AnushaAD <anusha.au011@gmail.com>
2020-09-04 18:07:19 +0530

    101-vm-simple-rhel

---
+++ 101-vm-simple-rhel/variables.tf
@@ -0,0 +1,62 @@
+# Authentication
+provider "azurerm" {
+  features {}
+  subscription_id = var.tf_var_arm_subscription_id
+  client_id       = var.tf_var_arm_client_id
+  client_secret   = var.tf_var_arm_client_secret
+  tenant_id       = var.tf_var_arm_tenant_id
+}
+
+# Variable declaration
+variable "tf_var_arm_subscription_id" {
+  description = "enter subscription id"
+}
+
+variable "tf_var_arm_client_id" {
+  description = "Enter Client ID"
+}
+
+variable "tf_var_arm_client_secret" {
+  description = "Enter secret"
+}
+
+variable "tf_var_arm_tenant_id" {
+  description = "Enter tenant ID"
+}
+
+variable "resourceGroupName" {
+  type        = string
+  default     = "demo-rg"
+  description = "Resource Group for this deployment."
+}
+
+variable "location" {
+  type        = string
+  default     = "westus"
+  description = "Enter the location for all resources."
+}
+
+variable "adminUsername" {
+  type        = string
+  description = "User name for the Virtual Machine"
+}
+
+variable "vmName" {
+  type        = string
+  description = "Name for the Virtual Machine"
+}
+
+variable "adminPasswordOrKey" {
+  type        = string
+  description = "SSH Key or password for the Virtual Machine. SSH key is recommended."
+}
+
+variable "authenticationType" {
+  type        = string
+  default     = "sshPublicKey"
+  description = "Type of authentication to be used on the Virtual Machine. SSH key is recommended but if you want to use password authentication use 'password'"
+  validation {
+    condition     = var.authenticationType == "sshPublicKey" || var.authenticationType == "password"
+    error_message = "Authentication type hould be either \"ssh\" or \"password\"."
+  }
+}
